8b86987d0dce98b4e1675a4642f9ddc5d590de20
h3canlao <vtp336@gmail.com>
2025-06-03 21:44:00 +0700

    Auth Part 2

--- src/services/apiServices.jsx
+++ src/services/apiServices.jsx
@@ -7,17 +7,31 @@ const postSignin = async (email, password) => {
 };
 
 const postRegister = async (firstName, lastName, email, password, passwordConfirm, gender) => {
-    return axios.post(`${BASE_URL}/Auth/register`, {
-        firstName,
-        lastName,
+  return axios.post(`https://localhost:7096/parent/registerUser`, {
+    firstName,
+    lastName,
+    email,
+    password,
+    passwordConfirm,
+    gender,
+  });
+};
+
+const forgetPassword = async (email) => {
+    return axios.post(`${BASE_URL}/Auth/forgot-password`, { email });
+};
+
+const resetPassword = async ({ email, token, newPassword }) => {
+    return axios.post(`${BASE_URL}/Auth/reset-password`, {
         email,
-        password,
-        passwordConfirm,
-        gender
+        token,
+        newPassword
     });
 };
 
 export {
     postSignin,
-    postRegister
+    postRegister,
+    forgetPassword,
+    resetPassword
 };
